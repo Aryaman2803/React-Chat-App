6e7c3d59637175ca79fa71226aa5563b51b56178
aryaman2803 <aryamansingh2803@gmail.com>
2021-03-29 23:38:50 +0530

    ChatContext

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -1,11 +1,48 @@
 import { useEffect } from "react";
 import { useChat } from "../../context/ChatContext";
-
+import { getChats, ChatEngine } from "react-chat-engine";
+//*We hook the Chat Engine here
 export const Chat = () => {
-  const {chatConfig} = useChat();
+  const { myChats, setMyChats, chatConfig, selectedChat } = useChat();
 
   useEffect(() => {
-    console.log(chatConfig);
-  }, [chatConfig]);
-  return <> I am the Chat</>;
+    console.log("My Chats: ", myChats);
+  }, [myChats]);
+  return (
+    <>
+      {/* If chatConfig exists then we render the ChatEngine */}
+      {!!chatConfig && (
+        <ChatEngine
+          hideUI={true}
+          userName={chatConfig.userName}
+          projectID={chatConfig.projectID}
+          userSecret={chatConfig.userSecret}
+          onConnect={() => {
+            //*It is a function which calls automatically whenever their is a successful connection to ChatEngine
+            //! This is a Web Socket Connection
+
+            //* getChats() takes the chatConfig and returns a Callback which gives us an Array of Chat.
+            //* So we will store the array in our setMyChats state
+            getChats(chatConfig, setMyChats);
+          }}
+        />
+      )}
+      <div className="chat-container">
+        <div className="current-chat">
+          {selectedChat ? (
+            <> </>
+          ) : (
+            <div className="no-chat-selected">
+              <img
+                src="/img/pointLeft.png"
+                className="point-left"
+                alt="point-left"
+              />
+              Select A Chat.
+            </div>
+          )}
+        </div>
+      </div>
+    </>
+  );
 };
